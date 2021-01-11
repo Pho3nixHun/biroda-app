b33af9797a5e9f32a73cb5ac27695e156a042361
László Simon <Pho3nixHun@users.noreply.github.com>
2021-01-11 21:35:15 +0100

    movie-tile component

--- app/client/src/app/tile/tile.component.ts
+++ app/client/src/app/tile/tile.component.ts
@@ -1,11 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+// TODO: Move this to service level or make it slimmer.
+export type Item = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  media_type: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
 
 @Component({
-  selector: 'app-tile',
+  selector: 'movie-tile',
   templateUrl: './tile.component.html',
   styleUrls: ['./tile.component.scss']
 })
 export class TileComponent implements OnInit {
+  @Input()
+  item?: Item
 
   constructor() { }
 
