28522f020c55f02a021fd62c8e79480fd37d6e5b
László Simon <Pho3nixHun@users.noreply.github.com>
2021-01-11 21:39:52 +0100

    tile-carousel

--- app/client/src/app/carousel/carousel.component.ts
+++ app/client/src/app/carousel/carousel.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+// TODO: Accept template instead of burned in movie-tile.
+import type { Item } from '../tile/tile.component';
+
+// TODO: Move this to service level or make it slimmer.
+export type Carousel = {
+  created_by: string;
+  description: string;
+  favorite_count: number;
+  id: string;
+  items: Item[];
+  item_count: number;
+  iso_639_1: string;
+  name: string;
+  poster_path: string;
+}
 
 @Component({
   selector: 'tile-carousel',
@@ -8,6 +24,16 @@ import { Component, OnInit } from '@angular/core';
 export class CarouselComponent implements OnInit {
 
   constructor() { }
+
+  @Input()
+  carousel?: Carousel;
+
+  @Output()
+  tileClick = new EventEmitter();
+
+  tileClickHandler(item: Item) {
+    this.tileClick.emit(item);
+  }
 
   ngOnInit(): void {
   }
