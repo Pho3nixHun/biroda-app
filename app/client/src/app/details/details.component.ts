7de099b3e9054b0390eecc3a558955db19de7a24
László Simon <Pho3nixHun@users.noreply.github.com>
2021-01-11 21:40:03 +0100

    details page

--- app/client/src/app/details/details.component.ts
+++ app/client/src/app/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+import { OnDemandService } from '../onDemand.service';
+import type { Item } from '../tile/tile.component';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
+  
+  id?: string;
+  details?: any;
+  
+  constructor(
+    private location: Location,
+    private router: Router, 
+    private route: ActivatedRoute, 
+    private onDemandService: OnDemandService
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  tileClicked($item: Item) {
+    this.router.navigate(['/details', $item.id]);
   }
 
+  back() {
+    this.location.back();
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = params.get('id') || this.id;
+      if (this.id) {
+        this.onDemandService.details(this.id).subscribe((data: any) => {
+          this.details = data;
+        });
+      }
+    });
+    
+  }
 }
